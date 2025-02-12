8056733ecdbf0395766d1255f81d5f05c8f3799b
MazenAbdelateef <mazenkahraba502@gmail.com>
2025-02-12 14:54:44 +0200

    OOP princples

--- week 9/OOP.php
+++ week 9/OOP.php
@@ -2,13 +2,13 @@
 class Song{
     public string $title;
     public string $artist;
+    public int $noOfplays = 0;
     public function __construct(string $title,string $artist){
         $this->title = $title;
         $this->artist = $artist;
+        $noOfplays++;
     }
 }
-
-
 class PlayList{
     public array $songs=[];
     function addNewSong(Song $song){
@@ -19,12 +19,32 @@ class PlayList{
             echo "The current song is: " . $song->title . " and the artist is: " . $song->artist . PHP_EOL;
         }
     }
+    function getSongCount(array $songs){
+        return count($songs);
+    }
 }
-$song1= new Song("s1","x1");
-$song2= new Song("s2","x2");  
-$song3= new Song("s3","x3");
-$playlist = new PlayList();
-$playlist->addNewSong($song1);
-$playlist->addNewSong($song2);
-$playlist->addNewSong($song3);
-$playlist->playSong();
+////////////////////////////////////////
+class Employee{
+    private string $name;
+    private int $id;
+    public function __construct(string $name, int $id){
+        $this->name = $name;
+        $this->id = $id;
+    }
+    function getBounus(){
+        return $name;
+    }
+}
+
+class Doctor extends Employee {
+    private string $Specialty;
+    private array $patients;
+    public function __construct(string $Specialty,string $patients){
+        parent::__construct($name, $id);
+        $this->Specialty = $Specialty;
+        $this->patients = $patients;
+    } 
+    function assignPatient(string $patient){
+        $this->patients[] = $patient;
+    }
+}
